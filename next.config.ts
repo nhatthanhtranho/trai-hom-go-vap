dd706341c6c53d867fef503c6865c68539c8c991
Nhat Thanh <nhatthanhtranho@gmail.com>
2025-10-19 16:43:06 +0700

    aaaa

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const isAWS = process.env.NEXT_PUBLIC_ENV === "AWS";
-
+const isProd = process.env.NEXT_PUBLIC_ENV === "PRODUCTION";
+const basePath = isProd ? "/trai-hom-go-vap" : "";
+const assetPrefix = isProd ? "/trai-hom-go-vap/" : isAWS ? process.env.NEXT_PUBLIC_CLOUDFRONT_URL : "";
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  // basePath: isProd ? "/trai-hom-go-vap" : "",
-  assetPrefix: isAWS ? process.env.NEXT_PUBLIC_CLOUDFRONT_URL : undefined,
+  basePath,
+  assetPrefix
 };
 
 export default nextConfig;
